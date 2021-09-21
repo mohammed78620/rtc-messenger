54024d9f099d4f2ecbd73d9ab74349b08d603aa1
mohammedmiah67890@gmail.com <mohammedmiah67890@gmail.com>
2021-09-21 21:03:50 +0100

    image uploaded implemented

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const socket = io('http://localhost:3000')
 const messageContainer = document.getElementById('message-container')
 const messageForm = document.getElementById('send-container')
 const messageInput = document.getElementById('message-input')
+const selectedFile = document.getElementById('send-button')
 
 const name = prompt('what is your name?')
 appendMessage(`you joined`)
@@ -10,18 +11,55 @@ socket.emit('new-user', name)
 socket.on('chat-message', data => {
     appendMessage(`${data.name}: ${data.message}`)
 })
+socket.on('file-message', data => {
+    console.log(data.file)
+    appendMessage('file recieved')
+    // readURL(data.file)
+    appendFile(data)
+    
+})
 socket.on('user-connection', name => {
     appendMessage(`${name} connected`)
 })
+socket.on('user-disconnection', name => {
+    appendMessage(`${name} disconnected`)
+})
+
 
 messageForm.addEventListener('submit', e => {
     e.preventDefault()
     const message = messageInput.value
+    appendMessage(`You: ${message}`)
     socket.emit('send-chat-message', message)
     messageInput.value = ''
 })
+function readURL(input) {
+    if (input.files && input.files[0]) {
+        var reader = new FileReader();
+        reader.onload = function (e) {
+            msg = {}
+            msg.src = e.target.result
+            img = document.createElement('img')
+            img.src = e.target.result
+            img.style.width="50%";
+            messageContainer.append(img)
+
+            socket.emit('send-file-message', msg);
+        };
+        
+
+        reader.readAsDataURL(input.files[0]);
+        
+    }
+}
 function appendMessage(message) {
     messageElement = document.createElement('div')
     messageElement.innerText = message
     messageContainer.append(messageElement)
+}
+function appendFile(message) {
+    img = document.createElement('img')
+    img.src = message.file
+    messageContainer.append(img)
+
 }
--- server.js
+++ server.js
@@ -4,6 +4,8 @@ const io = require('socket.io')(3000, {
       },
 })
 
+
+
 const users = {}
 
 io.on('connection', socket => {
@@ -15,4 +17,12 @@ io.on('connection', socket => {
     socket.on('send-chat-message', message => {
         socket.broadcast.emit('chat-message', {message: message, name: users[socket.id]})
     })
+    socket.on('send-file-message', message => {
+        socket.broadcast.emit('file-message', {file: message.src, name: users[socket.id]})
+    })
+    socket.on('disconnect', () => {
+        socket.broadcast.emit('user-disconnection', users[socket.id])
+        delete users[socket.id]
+    })
 })
+
